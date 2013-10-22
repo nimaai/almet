f271c0d813fc7d05ed2f1156a1d1365381dfc208
Matus Kmit <matus.kmit@zhdk.ch>
2013-10-22 18:47:40 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Almet::Application.config.secret_key_base = '2c87e9183b5a0272648ac5240e7831f4439c79bd1f0371cc3f3155e5019e159df0ec883edbc7b24a964fe3b820bf5dcaaba4aa32822fd74ed664f4e22751faf6'
