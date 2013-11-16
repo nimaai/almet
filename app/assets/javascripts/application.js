be39cdad4bc6ef773150108e8a2eccfdfe45402f
Matus Kmit <matus.kmit@zhdk.ch>
2013-11-16 22:37:19 +0100

    integration of bootstrap WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require sass-bootstrap/dist/js/bootstrap
+//= require sass-bootstrap/dist/js/bootstrap.min
