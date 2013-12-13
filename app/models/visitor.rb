fe7d6fe436ffad10662a57fdd68ceebdf74f8088
Matus Kmit <matus.kmit@zhdk.ch>
2013-12-13 18:01:00 +0100

    phone/mobile validations: either of them must be present

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,7 +1,8 @@
 class Visitor < ActiveRecord::Base
   has_many :reservations
 
-  validates_presence_of :firstname, :lastname, :street, :zip, :city, :country, :mobile, :email
+  validates_presence_of :firstname, :lastname, :street, :zip, :city, :country, :email
   validates_presence_of :phone, unless: :mobile?
+  validates_presence_of :mobile, unless: :phone?
   validates_uniqueness_of :email
 end
