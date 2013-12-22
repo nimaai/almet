7bf871bb764e711ff21fb587f0d02c58dbae1c35
Matus Kmit <matus.kmit@zhdk.ch>
2013-12-22 16:25:10 +0100

    added bootstrap form helpers to vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require sass-bootstrap/dist/js/bootstrap
+//= require bootstrap/dist/js/bootstrap
+//= require bootstrap-form-helpers/dist/js/bootstrap-formhelpers
