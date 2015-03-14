10bc8dbc01bf45d4d68dee413c50f01115f15653
Matus Kmit <matus.kmit@zhdk.ch>
2015-03-14 18:06:43 +0100

    test

--- test/controllers/visitors_controller_test.rb
+++ test/controllers/visitors_controller_test.rb
@@ -1,9 +1,26 @@
 class VisitorsControllerTest < ActionController::TestCase
+
   test 'index' do
     get :index
     visitors = assigns(:visitors)
     assert_equal \
       visitors.first,
       Visitor.all.sort_by(&:lastname).first
+  end
+
+  test 'update' do
+    visitor_attrs = { firstname:    'FirstnameX',
+                      lastname:     'LastnameX',
+                      street:       'StreetX 13',
+                      zip:          'ZipX',
+                      city:         'CityX',
+                      country:      'CountryX',
+                      mobile:       '0904123456',
+                      phone:        '0411234567' }
+
+    visitor = Visitor.all.sample
+    post :update, { id: visitor.id }.merge(visitor_attrs)
+    assert_redirected_to visitors_path
+    assert_not_nil flash[:success]
   end
 end
